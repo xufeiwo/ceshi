2e105af06b3c7dcf55ecce4c7ffdae978ea23343
徐飞 <364739377@qq.com>
2018-12-29 12:01:19 +0800

    w ss d yi

---
+++ demo.php
@@ -0,0 +1,5 @@
+<?php
+require_once './src/src/Tools.php';
+use src\Tools;
+$new=new Tools();
+$new->Hello();
---
+++ src/src/Tools.php
@@ -0,0 +1,7 @@
+<?php
+namespace src;
+class Tools{
+	public function Hello(){
+		echo "111111111";
+	}
+}
